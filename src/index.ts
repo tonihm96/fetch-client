c62f5ecea87686fd276aa6e6b74f66a0dfc4dc7b
Antonio Henrique Macan <toninhm96@gmail.com>
2025-12-28 21:21:25 -0300

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export { default as FetchClient } from "./fetch-client";
-export * from "./fetch-client";
+export { default as FetchClient } from './fetch-client';
+export * from './fetch-client';
+export * from './types';
+export * from './errors/fetch-client-error';
+export * from './errors/fetch-client-timeout-error';
